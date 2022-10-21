5c99e3782ce0e7d5e2cfe10eb01f6aac67b58185
shivamkumar <kumarshivam16062004@gmail.com>
2022-10-21 22:09:49 +0530

    version 1.0

---
+++ if_else_assignment.py
@@ -0,0 +1,21 @@
+houseprice=10000000
+you_are_having=10000001
+if you_are_having < houseprice:
+    print('You cannot afford')
+else:
+    print('The house is yours')
+    
+    
+    
+if (10000000000>500000000): print('Ten billion is greater than five hundred million')  
+
+
+A=int(input())
+B=int(input())
+
+print('I love you') if A>B else print('I hate you')  #one liner for if_else 
+    
+    
+    
+    
+    
---
+++ precedence.py
@@ -0,0 +1,29 @@
+# operator precedence
+# multilicatin has higher precedence over substraction 
+# print(10-4*2)
+# paranthesis ( has higher precedence over multiplication)
+# print((10-4)*2)
+# Associativity (left to right in python)
+
+# when two operators have same precedence then it helps us to determine the order of the operations. 
+# almost all the operators have left to right respectively
+
+
+# print(5**2//3)
+
+# print(5//2*3)
+# exponent operator has right to left associatevely
+# print(2**3**4)
+
+
+
+
+
+
+
+
+
+
+
+
+
